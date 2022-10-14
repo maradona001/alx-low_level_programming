33ac3dc16e50d066fcc48a654dffde0bda7ecc1e
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-14 22:50:15 +0000

    whats going on

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - print a line of code using puts
+ * main - print the string in the put function
  *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-ini main(void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
