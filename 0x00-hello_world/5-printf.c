41f62e52241809c68801664baacb30677ad3024f
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-14 23:00:28 +0000

    using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the using printf function
+ *
+ * Description: using the printf function
+ * this program prints "Programming gets better with practice
+ * Return: 0
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
