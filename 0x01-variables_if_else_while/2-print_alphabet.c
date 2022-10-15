a189146628eee837bdffad1f6f6d5c1896863994
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-15 10:15:41 +0000

    dont use numbers instead of letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,34 +2,32 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - print if the number is positive, zero, or  negavtive
+* main - print if the number is positive, zero, or negative
 *
 * Description: using the main function
-* this program prints "Programming is positive, zero, or negative
+* this program prints "programming is positive, zero or negative
 * Return: 0
 */
 int main(void)
 {
 int n;
-int 1;
+int m;
 
 srand(time(0));
-n = read() - RAND_MAX / 2;
-1 = n % 10;
+n = rand() - RAND_MAX / 2;
+m = n % 10;
 
-if (1 > 5)
+if (m > 5)
 {
-	printf("Last digit of %d is %d is greater than 5\n", n, 1);
+	printf("Last digit of %d is %d and is greater than 5\n", n, m);
 }
-else if (1 == 0)
+else if (m == 0)
 {
-	printf("Last digit of %d is %d  and is 0\n", n, 1);
+	printf("Last digit of %d is %d and is 0\n", n, m);
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
-return (0);
+	return (0);
 }
-
-
