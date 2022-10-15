8e32ee6568f69ef4e8ac8216466f020857298492
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-15 13:47:16 +0000

    priniting all single digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ *
+ * Description: Printing all sible numbers
+ * this program prints "all sinle numbers including zero
+ * Return: 0
+ */
+int main(void)
+{
+int f;
+for (f = 0; f < 10; f++)
+{
+	printf("%f", f);
+}
+printf("\n");
+return (0);
+}
