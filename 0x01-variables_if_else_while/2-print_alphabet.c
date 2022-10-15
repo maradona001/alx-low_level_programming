d72cefe7aec26a32c99d0f8dfb03b2fa8fbb7b68
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-15 09:29:19 +0000

    i found the mistake

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * main - print if the number is positive, zero, negavtive
 *
 * Description: using the main function
 * this programme prints "Programming is positive, zero, or negative
-* Return 0
+* Return: 0
 */
 int main(void)
 {
