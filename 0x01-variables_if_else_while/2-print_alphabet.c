0630d4050b3d1d483a352120285c0bc1acd991ba
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-15 12:32:41 +0000

    getting to print in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,35 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- * main - print if the number is positive, zero, or negative
+ * main - prints the a;phabets im lowercase
  *
- * Description: using the main function
- * this program prints "programming is positive, zero, or negative
  * Return: 0
  */
 int main(void)
 {
-int n;
-int k;
+	char letter;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-k = n % 10;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-if (k > 5)
-{
-	printf("Last digit of %d is %d and is greater than 5\n", n, k);
-}
-else if (k == o)
-{
-	printf("Last digit of %d is %d and id 0\n", n, k);
-}
-else
-{
-	print("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
-}
-return (0);
-}
+	putchar('\n');
 
-
+	return (0);
+}
