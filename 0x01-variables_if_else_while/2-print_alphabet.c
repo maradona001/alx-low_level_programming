87cc554361ec26a28b99a718de0b9e30118229d3
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-15 09:21:10 +0000

    its hard

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - print if the number is positive, zero, negavtive
+*
+* Description: using the main function
+* this programme prints "Programming is positive, zero, or negative
+* Return 0
+*/
+int main(void)
+{
+int n;
+int 1;
+
+srand(time(0));
+n = read() - RAND_MAX / 2;
+1 = n % 10;
+
+if (1 > 5)
+{
+	print("Last digit of %d is %d is greater than 5\n", n, 1);
+}
+else if (1 == 0)
+{
+	printf("Last digit of %d is %d  and is 0\n", n, 1);
+}
+else
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+}
+return (0);
+}
+
+
