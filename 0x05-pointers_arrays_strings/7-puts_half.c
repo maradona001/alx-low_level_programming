0a7e1b9b5f23df038ee1475c8bb6088aa942be1a
maradesa tofunmi samuel <samuelmaradesa@gmail.com>
2022-10-24 09:04:31 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,9 @@ void puts_half(char *str)
 
 	while (str[string] != '\0')
 	string++,
-	if (string + 1 % 2 != '0')
+		if (string + 1 % 2 != '0')
 	n = (string - 1) / 2;
-	else
+		else
 	n = (string / 2);
 	n++;
 
