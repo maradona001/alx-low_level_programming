69ef2ed9666cecd18f0bebeddcb0df981db07a94
root <samuelmaradesa@gmail.com>
2022-11-07 14:29:16 -0800

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - prints the minimum number of coins
- * to make change for an amount of money
- * @argc: holds the number of arguements passed
- * @argv: the array pointer that holds the arguement passed
- * Return: always 0 (success)
+* main - prints the min number of coins to make change
+* for an amount of money
+* @argc: argument count
+* @argv: arguments
+* Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-        int cents, coins_number = 0;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
-        if (argc != 2)
-        {
-        printf("Error\n");
-        return (1);
-        }
-        else
-        {
-        cents = atoi(argv[1]);
-        while (cents > 0)
-        {
-                if (cents >= 25)
-                        cents -= 25;
-                else if (cents >= 10)
-                        cents -= 10;
-                else if (cents >= 5)
-                        cents -= 5;
-                else if (cents >= 2)
-                        cents -= 2;
-                else if (cents >= 1)
-                        cents -= 1;
-                coins_number += 1;
-        }
-        }
-        printf("%d\n", coins_number);
-        return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = size(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
 }
-
